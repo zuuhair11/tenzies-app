50c457c04208eee0be44d3b0d2eb33325f9127d6
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-08-27 10:37:10 -0400

    added dice image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { nanoid } from 'nanoid' ;
+import { useEffect, useState } from 'react' ;
 import { useWindowSize } from 'react-use' ;
 import Confetti from 'react-confetti' ;
-import { useEffect, useState } from 'react' ;
 import Die from './components/Die' ;
 
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,11 +3,12 @@ function Die({ die, handleHoldDice }) {
         <div 
             className='die-face' 
             style={{
-                backgroundColor: die.isHeld === true && '#59E391'
+                backgroundColor: die.isHeld === true && '#59E391',
+                border: die.isHeld === true && '1px dotted gray '
             }}
             onClick={ () => handleHoldDice(die.id) }
         >
-            <h2 className='die-num'>{ die.value }</h2>
+            <img src={`../../assets/dice-${die.value}.png`} alt='' />
         </div>
     );
 }
